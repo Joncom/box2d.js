b184c0d32286fc3f7ece2662dcbe51941ec380a2
Jonathan Commins <joncom@gmail.com>
2013-06-13 22:34:50 -0700

    Added embind for b2MassData.center property.

--- box2d_embindings.cpp
+++ box2d_embindings.cpp
@@ -6,9 +6,14 @@ using namespace emscripten;
 float32 b2MassDataGetMass(const b2MassData& s) { return s.mass; }
 void b2MassDataSetMass(b2MassData& s, float32 mass) { s.mass = mass; }
 
+b2Vec2 b2MassDataGetCenter(const b2MassData& s) { return s.center; }
+void b2MassDataSetCenter(b2MassData& s, b2Vec2 center) { s.center = center; }
+
+
 EMSCRIPTEN_BINDINGS(box2d_embindings) {
 	class_<b2MassData>("b2MassData")
 		.constructor<>()
 		.property("mass", &b2MassDataGetMass, &b2MassDataSetMass)
+		.property("center", &b2MassDataGetCenter, &b2MassDataSetCenter)
 		;
 }
