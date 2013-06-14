b271db679a48d2039df7b8f39c04aac8d9013423
Jonathan Commins <joncom@gmail.com>
2013-06-13 19:38:51 -0700

    A test to make b2MassData accessible.

--- box2d_embindings.cpp
+++ box2d_embindings.cpp
@@ -1,7 +1,14 @@
 #include <emscripten/bind.h>
+#include "Box2D/Collision/Shapes/b2Shape.h"
 
 using namespace emscripten;
 
-EMSCRIPTEN_BINDINGS(box2d_embindings) {
+float32 b2MassDataGetMass(const b2MassData& s) { return s.mass; }
+void b2MassDataSetMass(b2MassData& s, float32 mass) { s.mass = mass; }
 
+EMSCRIPTEN_BINDINGS(box2d_embindings) {
+	class_<b2MassData>("b2MassData")
+		.constructor<>()
+		.property("mass", &b2MassDataGetMass, &b2MassDataSetMass)
+		;
 }
